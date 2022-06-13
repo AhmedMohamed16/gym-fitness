496d84c0beeb96be002f275c0d37a1eee0cfd999
AhmedMohamed16 <54655862+AhmedMohamed16@users.noreply.github.com>
2022-06-13 03:15:11 +0200

    first

---
+++ src/utils/fetchData.js
@@ -0,0 +1,22 @@
+export const exerciseOptions = {
+  method: 'GET',
+  headers: {
+    'X-RapidAPI-Host': 'exercisedb.p.rapidapi.com',
+    'X-RapidAPI-Key': '99c0a44b97msh5c7a441af509723p151a0cjsn1b348ab8cea4',
+  },
+};
+
+export const youtubeOptions = {
+  method: 'GET',
+  headers: {
+    'X-RapidAPI-Host': 'youtube-search-and-download.p.rapidapi.com',
+    'X-RapidAPI-Key': 'f0021db587msh781fb1cbef39856p11c183jsn45521d5d1c85',
+  },
+};
+
+export const fetchData = async (url, options) => {
+  const res = await fetch(url, options);
+  const data = await res.json();
+
+  return data;
+};
